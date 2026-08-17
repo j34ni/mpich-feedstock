1e4eee295a869d742c871b9123f6ce3f0fd59990
j34ni <j34ni@users.noreply.github.com>
2026-08-17 16:00:05 +0000

    Fix OMB build: set LD_LIBRARY_PATH for mpicc linking

--- recipe/build.sh
+++ recipe/build.sh
@@ -161,6 +161,8 @@ make install
 cd ../omb
 
 export PATH="${PREFIX}/bin:$PATH"
+export LD_LIBRARY_PATH="${PREFIX}/lib:${LD_LIBRARY_PATH:-}"
+export LIBRARY_PATH="${PREFIX}/lib"
 
 ./configure CC=mpicc CXX=mpicxx
 
